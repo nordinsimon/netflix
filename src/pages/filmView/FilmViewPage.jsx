cfc5d78424d55e1a69f69d201c05ef222089ff64
SkyscraperOnMars <maximus.nordgren@gmail.com>
2023-10-02 12:28:58 +0200

    render movie from activeMovie and bookmark button

--- src/pages/filmView/FilmViewPage.jsx
+++ src/pages/filmView/FilmViewPage.jsx
@@ -2,15 +2,16 @@ import { useContext } from "react";
 import AllContext from "../../context/context";
 import './filmViewPage.css'
 import moviesData from '../../../movies.json'
+import BookmarkButton from "../../../components/BookmarkButton";
 
 const FilmViewPage = () => {
-  const {bookmarks, setBookmarks, movies, activeMovie} = useContext(AllContext);
+  const {activeMovie} = useContext(AllContext);
 
   return (
     <div className="container">
     <h1>{activeMovie.title} ({activeMovie.year})</h1>
     <img src={activeMovie.thumbnail} alt={`${activeMovie.title} Thumbnail`} />
-    {/* <BookmarkButton movie={activeMovie} /> */}
+    <BookmarkButton movie={activeMovie} />
     <p><strong>Rating:</strong> {activeMovie.rating}</p>
     <p><strong>Genre:</strong> {activeMovie.genre}</p>
     <ul className="actors">
