bb62ccdd5fae09517e784fe7f07c3ca4fc83a154
nordinsimon <simon.s.nordin@gmail.com>
2023-10-04 22:50:06 +0200

    Added Integration test for login

---
+++ src/__IntegrationTestsApp__/AppCategory.test.jsx
@@ -0,0 +1,33 @@
+import { test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { AllContextProvider } from "../context/context";
+import { MemoryRouter } from "react-router-dom";
+
+import App from "../App";
+
+const custumRender = () => {
+  return render(
+    <AllContextProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </AllContextProvider>,
+  );
+};
+
+const loginTestFunction = async () => {
+  custumRender();
+  const username = screen.getByPlaceholderText("Username");
+  const password = screen.getByPlaceholderText("Password");
+  const button = screen.getByRole("button");
+  await userEvent.type(username, "Simon");
+  await userEvent.type(password, "123");
+  await userEvent.click(button);
+};
+
+test("that category button exists", async () => {
+  await loginTestFunction();
+  const category = screen.getByText("Category");
+  expect(category).to.exist;
+});
---
+++ src/__IntegrationTestsApp__/AppLogin.test.jsx
@@ -0,0 +1,60 @@
+import { test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { AllContextProvider } from "../context/context";
+import { MemoryRouter } from "react-router-dom";
+
+import App from "../App";
+
+const custumRender = () => {
+  return render(
+    <AllContextProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </AllContextProvider>,
+  );
+};
+
+const loginTestFunction = async () => {
+  custumRender();
+  const header = screen.getByText("Welcome back!");
+  const username = screen.getByPlaceholderText("Username");
+  const password = screen.getByPlaceholderText("Password");
+  const button = screen.getByRole("button");
+
+  expect(header).to.exist;
+  expect(username).to.exist;
+  expect(password).to.exist;
+
+  await userEvent.type(username, "Simon");
+  await userEvent.type(password, "123");
+  await userEvent.click(button);
+};
+
+test("that error massage is working when typing wrong username or password", async () => {
+  custumRender();
+  const header = screen.getByText("Welcome back!");
+  const username = screen.getByPlaceholderText("Username");
+  const password = screen.getByPlaceholderText("Password");
+  const button = screen.getByRole("button");
+
+  expect(header).to.exist;
+  expect(username).to.exist;
+  expect(password).to.exist;
+
+  await userEvent.type(username, "Fel");
+  await userEvent.type(password, "321");
+  await userEvent.click(button);
+
+  const error = screen.getByText("Wrong username or password");
+  expect(error).to.exist;
+});
+
+test("that it is possible to login", async () => {
+  await loginTestFunction();
+  const trending = screen.getByText("Trending");
+  const recommended = screen.getByText("Recommended");
+  expect(trending).to.exist;
+  expect(recommended).to.exist;
+});
